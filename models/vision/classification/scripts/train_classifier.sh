370446b68e3dca9e4c40dce406d608fbdb057204
Kevin Yang <kevinyang8@berkeley.edu>
2020-06-15 18:55:29 +0000

    updates to file organization

---
+++ models/vision/classification/scripts/train_classifier.sh
@@ -0,0 +1,24 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#!/usr/bin/env bash
+
+# change -np and localhost: to number of gpus
+
+cd /deep-learning-models/models/vision/classification
+export PYTHONPATH=${PYTHONPATH}:${PWD}
+
+mpirun -np 8 \
+--H localhost:8 \
+--allow-run-as-root \
+-mca btl ^openib \
+-mca btl_tcp_if_exclude tun0,docker0,lo \
+--bind-to none \
+-x NCCL_SOCKET_IFNAME=^docker0,lo \
+-x NCCL_DEBUG=INFO \
+python tools/train_backbone.py \
+--model darknet53 \
+--train_data_dir /deep-learning-models/models/vision/classification/data/imagenet/train \
+--validation_data_dir /deep-learning-models/models/vision/classification/data/imagenet/validation \
+--batch_size 256
+
+
